1f19495ddbe68db049369df78d3420c87c144112
GabrielLima01 <gabriellima0218@hotmail.com>
2021-11-12 21:20:59 -0300

    Criação do Evento.js, Form.js e Button.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,17 +1,30 @@
+import { useState } from 'react';
 function Form() {
 
 
     function cadastrarUsuario(e) {
         e.preventDefault()  //Para a execução do formulário, e executa a linha de código abaixo
-        console.log('Usuário cadastrado!');
+        console.log(`Usuário ${name} foi cadastrado com a senha: ${password}`)
+        console.log('Usuário cadastrado!')
     }
+
+   const [name, setName] = useState()
+   const [password, setPassword] = useState()
 
     return (
         <div>
             <h1>Cadastro:</h1>
             <form onSubmit={cadastrarUsuario}>
                 <div>
-                    <input type="text" placeholder="Digite seu nome" />
+                    <label htmlFor="name">Nome:</label>
+                    <input type="text"
+                     placeholder="Digite seu nome"  
+                    onChange={(e) => setName(e.target.value)} />
+                </div>
+                <div>
+                <label htmlFor="password">Senha:</label>
+                    <input type="password" placeholder="Digite sua senha" 
+                    onChange={(e) => setPassword(e.target.value)}/>
                 </div>
                 <div>
                     <input type="submit" value="Cadastrar" />
